c113863c5de00af75b51f1f15901b44b6f628644
Matthew Green <matt.green@mead354.org>
2017-03-13 22:08:28 -0700

    All of them.  Working fade in of names…

--- js/functions.js
+++ js/functions.js
@@ -5,9 +5,15 @@ $(document).ready(function() {
         success : function (data) {
           var res = "";
           for (var i = 0; i < data.length; i++) {
-            res = res + data[i][0]+" "+data[i][1]+",";
+            res = res + data[i][1]+" "+data[i][0]+",";
           }
-          $(".text").html(res);
+          $(".rotate").html(res);
         }
     });
+
+    $(".rotate").textrotator({
+  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  speed: 2000 // How many milliseconds until the next word show.
+  });
 });
